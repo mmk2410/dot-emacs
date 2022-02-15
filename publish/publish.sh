3dbc64f11a01147548e5e787c08acb46744a1cde
Marcel Kapfer <opensource@mmk2410.org>
2022-02-15 19:58:26 +0100

    Make publish script workable from any directory

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-emacs -Q --script ./publish.el
+set -euo pipefail
+
+PUBLISH_SCRIPT_PATH=./publish/publish.el
+EMACS_CONF_DIR=~/.emacs.d
+CURRENT_DIR=$(pwd)
+
+function goBack() {
+    cd "$CURRENT_DIR"
+}
+trap goBack EXIT
+
+cd "$EMACS_CONF_DIR" || exit
+emacs -Q --script ./publish/publish.el
