b20f578afaed641a30d5fb76f28dcadac90f21ed
Marcel Kapfer <opensource@mmk2410.org>
2022-03-30 16:54:22 +0200

    [tab-bar-helper] Make hydra a lot more beautiful

--- packages/mmk2410-tab-bar-helpers.el
+++ packages/mmk2410-tab-bar-helpers.el
@@ -63,14 +63,28 @@
    #'(lambda ()
        (find-file "~/org/work.projects.org"))))
 
-(defhydra mmk2410/tab-bar (:color teal)
-  "My tab-bar helpers"
-  ("a" mmk2410/tab-bar-run-agenda "Agenda")
-  ("e" mmk2410/tab-bar-run-elfeed "RSS (Elfeed)")
-  ("i" mmk2410/tab-bar-run-irc "IRC (erc)")
-  ("j" mmk2410/tab-bar-run-journal "Journal")
-  ("m" mmk2410/tab-bar-run-mail "Mail")
-  ("p" mmk2410/tab-bar-run-projects "Projects"))
+(defhydra mmk2410/tab-bar (:color teal :hint nil)
+  "
+  ^Apps^               ^Helpers^        ^Misc
+--^^^^^^---------------------------------------------
+  _a_: Agenda          _RET_: Search    _q_: Cancel
+  _e_: RSS (Elfeed)    _SPC_: New
+  _i_: IRC (erc)       _f_: Previous
+  _j_: Journal         _Q_: Close
+  _m_: Mail
+  _p_: Projects
+"
+  ("a" mmk2410/tab-bar-run-agenda)
+  ("e" mmk2410/tab-bar-run-elfeed)
+  ("i" mmk2410/tab-bar-run-irc)
+  ("j" mmk2410/tab-bar-run-journal)
+  ("m" mmk2410/tab-bar-run-mail)
+  ("p" mmk2410/tab-bar-run-projects)
+  ("RET" tab-bar-select-tab-by-name)
+  ("SPC" mmk2410/tab-bar-new-named-tab)
+  ("f" tab-bar-switch-to-recent-tab)
+  ("Q" tab-bar-close-tab)
+  ("q" nil))
 
 (provide 'mmk2410-tab-bar-helpers)
 
